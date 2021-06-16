2b509de9b44e897d92ea1233b035436cd916260f
Guilhas <guilhaxr123@gmail.com>
2021-06-16 16:27:26 +0100

    first commit

---
+++ wilders.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+console.log(chalk.green('Hello'));
+console.log(chalk.red('World'));
+console.log(chalk.cyan('My name is'));
+console.log(chalk.bgBlueBright('G'));
